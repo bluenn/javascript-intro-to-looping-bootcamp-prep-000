05494b79ea622edfd492a675bcd11f43ae69f36d
a lan <bluenowte97@gmail.com>
2018-10-11 21:06:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -7,3 +7,21 @@ function whileLoop(n) {
 var string = “done”;
 return string;
 }
+
+
+function forLoop(array) {
+var string1 = "I am 1 strange loop.";
+ var stringOther = "I am ${i} strange loops.";
+ for (var i = 0; i < 25, i++){
+   if (i === 1) {
+     array.push(string1); 
+   }
+   else {
+     array.push(stringOther); 
+   }
+ }
+ return array;   
+  
+}
+  
+}
