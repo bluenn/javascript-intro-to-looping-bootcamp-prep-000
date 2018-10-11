40de669c5459ef0f53943f6a8205be4d92d9bcca
a lan <bluenowte97@gmail.com>
2018-10-11 20:55:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 
 function whileLoop(n) {
-
-  while (n > 0) {
-    console.log(--n);
-    n--;
+var number = 10;
+  while (number > 0) {
+    console.log(--number);
+    number--;
   }
 var string = “done”;
 return string;
