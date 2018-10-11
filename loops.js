b4c5abcd43560b4bed406907914429433140c048
a lan <bluenowte97@gmail.com>
2018-10-11 21:22:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,19 +12,19 @@ return string;
 // function forLoop(array) {
 //  //var string1 = "I am 1 strange loop.";
 //  //var stringOther = "I am ${i} strange loops.";
-// 
+//
 //   for (var i = 0; i < 25; i++){
 //     var string1 = "I am 1 strange loop."";
 //     var stringOther = "I am ${i} strange loops.";
-// 
+//
 //     if (i === 1) {
 //       array.push(string1);
 //     }
-// 
+//
 //    else {
 //      array.push(stringOther);
 //    }
-// 
+//
 //   }
 //   return array;
 // }
